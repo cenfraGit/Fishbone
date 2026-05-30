6811e9cbfd38183d86ade030f77d721366bd5bc2
cenfraGit <ceniceros.angel@hotmail.com>
2026-05-30 14:13:46 -0600

    feat: add FishboneEnvironment

---
+++ Fishbone.Core/FishboneEnvironment.cs
@@ -0,0 +1,34 @@
+namespace Fishbone.Core;
+
+public class FishboneEnvironment
+{
+    private readonly Dictionary<string, object> _values = [];
+
+    public FishboneEnvironment()
+    {
+    }
+
+    public void Declare(string name, object value)
+    {
+        if (_values.ContainsKey(name))
+            throw new Exception($"Variable \"{name}\" is already declared.");
+
+        _values[name] = value;
+    }
+
+    public void Assign(string name, object value)
+    {
+        if (!_values.ContainsKey(name))
+            throw new Exception($"Undefined variable \"{name}\"");
+
+        _values[name] = value;
+    }
+
+    public object GetValue(string name)
+    {
+        if (!_values.ContainsKey(name))
+            throw new Exception($"Undefined variable \"{name}\"");
+
+        return _values[name];
+    }
+}
