2ae0af0f6ac38a67805073fc983684bbd1694246
cenfraGit <ceniceros.angel@hotmail.com>
2026-05-31 16:29:23 -0700

    test: add test for environment and delegate execution

--- Fishbone.Engine.Tests/UnitTest1.cs
+++ Fishbone.Engine.Tests/UnitTest1.cs
@@ -3,8 +3,17 @@
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void Engine_CSharpDelegateBridge_InvokesAndMarshalsTypesCorrectly()
     {
+        var config = new FishboneConfiguration()
+            .RegisterFunction("formatValue", new Func<double, int, string>((val, precision) =>
+                val.ToString($"F{precision}")));
+        string code = @"
+            let scriptResult = formatValue(42, 3);
+        ";
 
+        var finalEnv = FishboneEngine.Run(code, config);
+
+        Assert.Equal("42.000", finalEnv.GetValue("scriptResult"));
     }
 }
