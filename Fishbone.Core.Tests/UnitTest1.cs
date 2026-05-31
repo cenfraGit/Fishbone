561078f1643c0c392478a11aa3b99aae10b0ec34
cenfraGit <ceniceros.angel@hotmail.com>
2026-05-31 14:51:55 -0700

    test: add Core.Tests and Engine.Tests

---
+++ Fishbone.Core.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Fishbone.Core.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ Fishbone.Engine.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Fishbone.Engine.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
