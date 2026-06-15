4d8897b2b99097062d4f8202b28bb1917e9a62b3
cenfraGit <ceniceros.angel@hotmail.com>
2026-06-14 21:30:10 -0700

    feat: add RegisterDefaults for standard-library-like functionality

--- src/Fishbone.Core/FishboneEnvironment.cs
+++ src/Fishbone.Core/FishboneEnvironment.cs
@@ -6,22 +6,77 @@ public class FishboneEnvironment
     private readonly Dictionary<string, object> _values = [];
     public Dictionary<string, object> Values { get => _values; }
 
+    public TextReader StdIn { get; set; } = Console.In;
     public TextWriter StdOut { get; set; } = Console.Out;
 
-    public FishboneEnvironment(FishboneEnvironment? parent = null)
+    public FishboneEnvironment(FishboneEnvironment? parent = null, bool injectDefaults = true)
     {
         _parent = parent;
 
-        if (parent is null)
-            RegisterCoreLibraries();
+        if (parent is null && injectDefaults)
+            RegisterDefaults();
     }
 
-    private void RegisterCoreLibraries()
+    private void RegisterDefaults()
     {
-        _values["print"] = new Action<object?>(value =>
+        // constants
+        _values["PI"] = Math.PI;
+        _values["E"] = Math.E;
+
+        // input/output
+        _values["println"] = new Action<object?>(value => { StdOut.Write(value?.ToString()); }); // print line
+        _values["print"] = new Action<object?>(value => { StdOut.WriteLine(value?.ToString()); });
+        _values["input"] = new Func<string>(() => StdIn.ReadLine() ?? string.Empty);
+
+        // basic math
+        _values["abs"]   = new Func<double, double>(Math.Abs);
+        _values["round"] = new Func<double, int, double>(Math.Round);
+        _values["min"]   = new Func<double, double, double>(Math.Min);
+        _values["max"]   = new Func<double, double, double>(Math.Max);
+        _values["pow"]   = new Func<double, double, double>(Math.Pow);
+        _values["sqrt"]  = new Func<double, double>(Math.Sqrt);
+
+        // int(someValue)
+        _values["int"] = new Func<object?, int>(value =>
         {
-            StdOut.WriteLine(value?.ToString());
+            if (value is null) return 0;
+            if (value is string str)
+            {
+                if (double.TryParse(str, out double strDouble))
+                    return (int)strDouble;
+                return int.TryParse(str, out int strInt) ? strInt : 0;
+            }
+            // change type
+            try
+            {
+                return (int)Convert.ChangeType(value, typeof(int));
+            }
+            catch
+            {
+                return 0;
+            }
         });
+
+        // double(someValue)
+        _values["double"] = new Func<object?, double>(value =>
+        {
+            if (value is null) return 0.0;
+            if (value is string str)
+                if (double.TryParse(str, out double strDouble))
+                    return (int)strDouble;
+
+            try
+            {
+                return (double)Convert.ChangeType(value, typeof(double));
+            }
+            catch
+            {
+                return 0.0;
+            }
+        });
+
+        // string(someValue)
+        _values["string"] = new Func<object?, string>(value => value?.ToString() ?? string.Empty);
     }
 
     public void Declare(string name, object value)
