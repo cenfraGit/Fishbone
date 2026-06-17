34d00313fbcb57f07ceb15f74583da6bc79d0146
MiguelCeniceros <mceniceros@vistasolutions.ca>
2026-06-16 21:18:51 -0600

    test: update with new method names

--- tests/Fishbone.Engine.Tests/FishboneConfigurationTests.cs
+++ tests/Fishbone.Engine.Tests/FishboneConfigurationTests.cs
@@ -3,14 +3,14 @@ namespace Fishbone.Engine.Tests;
 public class FishboneConfigurationTests
 {
     [Fact]
-    public void RegisterVariable_AddsValueAndReturnsSameConfiguration()
+    public void RegisterBuiltIn_AddsValueAndReturnsSameConfiguration()
     {
         var config = new FishboneConfiguration();
 
-        var returned = config.RegisterVariable("answer", 42);
+        var returned = config.RegisterBuiltIn("answer", 42);
 
         Assert.Same(config, returned);
-        Assert.Equal(42, config.Globals["answer"]);
+        Assert.Equal(42, config.BuiltIns["answer"]);
     }
 
     [Fact]
@@ -22,18 +22,18 @@ public class FishboneConfigurationTests
         var returned = config.RegisterFunction("doubleValue", doubleValue);
 
         Assert.Same(config, returned);
-        Assert.Same(doubleValue, config.Globals["doubleValue"]);
+        Assert.Same(doubleValue, config.BuiltIns["doubleValue"]);
     }
 
     [Fact]
-    public void Globals_UsesOrdinalKeyComparison()
+    public void BuiltIns_UsesOrdinalKeyComparison()
     {
-        var config = new FishboneConfiguration()
-            .RegisterVariable("Name", 1)
-            .RegisterVariable("name", 2);
+        var config = new FishboneConfiguration(injectDefaults: false)
+            .RegisterBuiltIn("Name", 1)
+            .RegisterBuiltIn("name", 2);
 
-        Assert.Equal(1, config.Globals["Name"]);
-        Assert.Equal(2, config.Globals["name"]);
-        Assert.Equal(2, config.Globals.Count);
+        Assert.Equal(1, config.BuiltIns["Name"]);
+        Assert.Equal(2, config.BuiltIns["name"]);
+        Assert.Equal(2, config.BuiltIns.Count);
     }
 }
--- tests/Fishbone.Engine.Tests/FishboneEngineTests.cs
+++ tests/Fishbone.Engine.Tests/FishboneEngineTests.cs
@@ -19,7 +19,7 @@ let y = x * 2;
     public void Run_SeedsRegisteredVariablesForScriptUse()
     {
         var config = new FishboneConfiguration()
-            .RegisterVariable("baseValue", 10);
+            .RegisterBuiltIn("baseValue", 10);
 
         var env = FishboneEngine.Run("""
 let result = baseValue + 5;
@@ -33,7 +33,7 @@ let result = baseValue + 5;
     public void Run_ReturnedEnvironmentRetainsTopLevelDeclarationsAndGlobals()
     {
         var config = new FishboneConfiguration()
-            .RegisterVariable("globalValue", 3);
+            .RegisterBuiltIn("globalValue", 3);
 
         var env = FishboneEngine.Run("""
 let scriptValue = globalValue + 7;
