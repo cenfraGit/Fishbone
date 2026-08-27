642e02713026ef0be85185e6fae103439b8d05df
cenfraGit <cenfra_ma@hotmail.com>
2026-08-26 19:36:16 -0600

    feat: add Fishbone.Plugins.Math

---
+++ plugins/Fishbone.Plugins.Math/MathPlugin.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------
+// MathPlugin.cs
+//
+// adds basic math functionality to a fishbone script.
+// --------------------------------------------------------------------------------
+
+namespace Fishbone.Plugins.Math;
+
+public sealed class MathPlugin : IFishbonePlugin
+{
+    public void Register(FishboneConfiguration config)
+    {
+        // constants
+        config.BuiltIns["PI"] = System.Math.PI;
+        config.BuiltIns["E"] = System.Math.E;
+
+        config.BuiltIns["abs"] = new Func<double, double>(System.Math.Abs);
+        config.BuiltIns["round"] = new Func<double, int, double>(System.Math.Round);
+        config.BuiltIns["min"] = new Func<double, double, double>(System.Math.Min);
+        config.BuiltIns["max"] = new Func<double, double, double>(System.Math.Max);
+        config.BuiltIns["pow"] = new Func<double, double, double>(System.Math.Pow);
+        config.BuiltIns["sqrt"] = new Func<double, double>(System.Math.Sqrt);
+    }
+}
